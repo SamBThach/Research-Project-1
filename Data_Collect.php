52745ce3fd637a1b96099bcf5e31d76098d86531
SamBThach <71841306+SamBThach@users.noreply.github.com>
2021-02-28 13:30:03 -0800

    Add files via upload

---
+++ Data_Collect.php
@@ -0,0 +1,45 @@
+<html>
+<?php 
+	$host = '127.0.0.1';
+	$username = 'root';
+	$password = '';
+			
+	$connection = mysqli_connect($host,$username,$password,"research project");
+			
+	if(!$connection){
+		echo 'No Connection';
+	}
+	if(!mysqli_select_db($connection, 'research project')){
+		echo 'No database';
+	}
+			
+	$Input_One = $_POST['Cybersecurity'];
+	$Input_Two = $_POST['Applied Maths'];
+	$Input_Three = $_POST['Software Engineering'];
+	$Input_Four  = $_POST['Embedded Systems'];
+	$Input_Five  = $_POST['Data Science'];
+	$Input_Six  = $_POST['Computer Engineering'];
+	$Time_Stamp = date(DATE_RFC2822);
+						
+	if($Input_One != $Input_Two || $Input_One != $Input_Three || $Input_One != $Input_Four || $Input_One != $Input_Five || $Input_One != $Input_Six
+				|| $Input_Two != $Input_Three ||$Input_Two != $Input_Four || $Input_Two != $Input_Five || $Input_Two != $Input_Six || $Input_Three != $Input_Four
+				||$Input_Three != $Input_Five || $Input_Three != $Input_Six || $Input_Four != $Input_Five || $Input_Four != $Input_Six || $Input_Five != $Input_Six
+				||$Input_One != "unselected" || $Input_One != "unselected" || $Input_One != "unselected" || $Input_One != "unselected" || $Input_One != "unselected"
+				||$Input_Two != "unselected" || $Input_Two != "unselected" || $Input_Two != "unselected" || $Input_Two != "unselected" || $Input_Three != "unselected"
+				||$Input_Three != "unselected" || $Input_Three != "unselected" || $Input_Four != "unselected" || $Input_Four != "unselected" || $Input_Five != "unselected"){
+		$sql = "INSERT INTO user_answers(User_Answer_One,User_Answer_Two,User_Answer_Three,User_Answer_Four,User_Answer_Five,User_Answer_Six,Time_Stamp) VALUES('$Input_One','$Input_Two','$Input_Three','$Input_Four','$Input_Five','$Input_Six','$Time_Stamp')";
+		if(!mysqli_query($connection,$sql)){
+			echo 'Values Not Collected';
+		}
+		else{
+			echo 'Values Collected';
+			echo date(DATE_RFC2822);
+		}
+		header("Location: Submitted Page.html");
+		exit();
+	}
+	else{
+		exit();
+	}
+?>
+</html>
