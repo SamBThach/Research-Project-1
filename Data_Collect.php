f57f07be2795e7dafc545189ad12b545ffa153a6
SamBThach <71841306+SamBThach@users.noreply.github.com>
2021-03-16 11:07:12 -0700

    Add files via upload

--- Data_Collect.php
+++ Data_Collect.php
@@ -1,21 +1,36 @@
 <html>
 <?php 		
+	$host = "127.0.0.1";
+	$username = "root";
+	$password = "";
+	
+	$connection = mysqli_connect($host, $username, $password, "research");
+	
+	if(!$connection){
+		echo "No connection";
+	}
+	if(!mysqli_select_db($connection, "research")){
+		echo "No database";
+	}
+	
 	$Input_One = $_POST['Cybersecurity'];
-	$Input_Two = $_POST['Applied Maths'];
-	$Input_Three = $_POST['Software Engineering'];
-	$Input_Four  = $_POST['Embedded Systems'];
-	$Input_Five  = $_POST['Data Science'];
-	$Input_Six  = $_POST['Computer Engineering'];
+	$Input_Two = $_POST['Applied_Maths'];
+	$Input_Three = $_POST['Software_Engineering'];
+	$Input_Four  = $_POST['Embedded_Systems'];
+	$Input_Five  = $_POST['Data_Science'];
+	$Input_Six  = $_POST['Computer_Engineering'];
 	$Time_Stamp = date(DATE_RFC2822);
 						
-	if($Input_One != $Input_Two || $Input_One != $Input_Three || $Input_One != $Input_Four || $Input_One != $Input_Five || $Input_One != $Input_Six
-				|| $Input_Two != $Input_Three ||$Input_Two != $Input_Four || $Input_Two != $Input_Five || $Input_Two != $Input_Six || $Input_Three != $Input_Four
-				||$Input_Three != $Input_Five || $Input_Three != $Input_Six || $Input_Four != $Input_Five || $Input_Four != $Input_Six || $Input_Five != $Input_Six
-				||$Input_One != "unselected" || $Input_One != "unselected" || $Input_One != "unselected" || $Input_One != "unselected" || $Input_One != "unselected"
-				||$Input_Two != "unselected" || $Input_Two != "unselected" || $Input_Two != "unselected" || $Input_Two != "unselected" || $Input_Three != "unselected"
-				||$Input_Three != "unselected" || $Input_Three != "unselected" || $Input_Four != "unselected" || $Input_Four != "unselected" || $Input_Five != "unselected"){
-		$sql = "INSERT INTO user_answers(User_Answer_One,User_Answer_Two,User_Answer_Three,User_Answer_Four,User_Answer_Five,User_Answer_Six,Time_Stamp) VALUES('$Input_One','$Input_Two','$Input_Three','$Input_Four','$Input_Five','$Input_Six','$Time_Stamp')";
-		$myfile = fopen("testfile.txt", "a+");
+	echo $Input_One;
+	echo $Input_Twp;
+	echo $Input_Three;
+	echo $Input_Four;
+	echo $Input_Five;
+	echo $Input_Six;
+	echo $Time_Stamp;
+	
+		$sql = "INSERT INTO user_inputs(Input1,Input2,Input3,Input4,Input5,Input6,Timestamp) VALUES('$Input_One','$Input_Two','$Input_Three','$Input_Four','$Input_Five','$Input_Six','$Time_Stamp')";
+		
 		if(!mysqli_query($connection,$sql)){
 			echo 'Values Not Collected';
 		}
@@ -24,11 +39,6 @@
 			echo date(DATE_RFC2822);
 		}
 		header("Location: Submitted Page.html");
-		fclose($myfile);
 		exit();
-	}
-	else{
-		exit();
-	}
 ?>
 </html>
