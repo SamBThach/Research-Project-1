1916c1fb4dcfc62ecee338fa29a6f7b88595453e
Jon Kelley <jon.kelley@oit.edu>
2021-04-16 14:29:34 -0700

    Edited Data_Collect and Research website to connect to db

--- Data_Collect.php
+++ Data_Collect.php
@@ -1,32 +1,41 @@
 <html>
 <?php 		
 
+	error_reporting(E_ALL);
+	ini_set('display_errors', '1');
+
 	include("Credentials.php");
 		
-		if(!$connection){
-					echo "No connection";
-							header("Location: https://csetplacement.cset.oit.edu/Errored_Research_Page.html");
-							exit();
-								}
-		else{
-					$Input_One = $_POST['Cybersecurity'];
-							$Input_Two = $_POST['Applied_Maths'];
-							$Input_Three = $_POST['Software_Engineering'];
-									$Input_Four = $_POST['Embedded_Systems'];
-									$Input_Five = $_POST['Data_Science'];
-											$Input_Six = $_POST['Computer_Engineering'];
-											$Time_Stamp = date(DATE_RFC2822);
+	if(!$connection)
+	{
+			echo "No connection";
+			header("Location: https://csetplacement.cset.oit.edu/Errored_Research_Page.html");
+			exit();
+	}
+	else
+	{
+		$Input_One = $_POST['Cybersecurity'];
+		$Input_Two = $_POST['Applied_Maths'];
+		$Input_Three = $_POST['Software_Engineering'];
+		$Input_Four = $_POST['Embedded_Systems'];
+		$Input_Five = $_POST['Data_Science'];
+		$Input_Six = $_POST['Computer_Engineering'];
+		$Time_Stamp = date('Y-m-d H:i:s');
 																
-													$sql = "INSERT INTO user_inputs(Input1,Input2,Input3,Input4,Input5,Input6,Timestamp) VALUES('$Input_One','$Input_Two','$Input_Three','$Input_Four','$Input_Five','$Input_Six','$Time_Stamp')";
+		$sql = "INSERT INTO user_inputs(Input1,Input2,Input3,Input4,Input5,Input6,Timestamp) VALUES('$Input_One','$Input_Two','$Input_Three','$Input_Four','$Input_Five','$Input_Six','$Time_Stamp')";
 													
-													if(!mysqli_query($connection,$sql)){
-																	echo 'Values Not Collected';
-																				echo date(DATE_RFC2822);
-																				header("Location: https://csetplacement.cset.oit.edu/Errored_Research_Page.html");
-																							exit();
-																						}
-															header("Location: https://csetplacement.cset.oit.edu/Submitted_Page.html");
-															exit();		
-																}
+		if(!mysqli_query($connection,$sql))
+		{
+			echo 'Values Not Collected';
+			echo date('Y-m-d H:i:s');
+			header("Location: https://csetplacement.cset.oit.edu/Errored_Research_Page.html");
+			exit();
+		}
+		else
+		{
+			header("Location: https://csetplacement.cset.oit.edu/Submitted_Page.html");
+			exit();
+		}
+	}														
 ?>
 </html>
